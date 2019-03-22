02c8690f1638d1050a109dbd4c41eb1bae30dd70
kspayne93 <kyle@investavante.com>
2019-03-22 17:54:41 -0600

    added timer to test & logic to calculate wpm

---
+++ src/components/TypingTest/TypingTest.jsx
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import './TypingTest.scss';
+import axios from 'axios';
+
+export default class TypingTest extends Component {
+   state = {
+      originalText: '',
+      originalTextArray: [],
+      userInput: '',
+      userInputArray: [],
+      time: 0,
+      start: 0,
+      isOn: false,
+      wordCount: 0,
+   }
+
+   componentDidMount = async () => {
+      await this.getParagraph();
+      const { originalText } = this.state;
+      await console.log(originalText.split(' ').length + ' words');
+   }
+
+   getParagraph = async () => {
+      const { originalText } = this.state;
+      const paragraph_id = await Math.floor((Math.random() * 14) + 1); //Picks a random paragraph from database
+      const res = await axios.get(`/api/paragraph/${paragraph_id}`);
+      await this.setState({ 
+         originalText: res.data[0].paragraph_text,
+         wordCount: originalText.split(' ').length,
+      });
+      await this.setState({ originalTextArray: this.state.originalText.split('') });
+   }
+
+   handleInput = async (inputString) => {
+      const { userInput } = this.state;
+      await this.setState({ userInput: inputString });
+      await this.setState({ userInputArray: userInput.split('') })
+      if (this.state.userInput.length === 1) {
+         this.startTimer();
+      } else if (this.state.userInput === this.state.originalText) {
+         this.stopTimer();      }
+   }
+
+   startTimer = async () => {
+      await this.setState({
+         time: this.state.time,
+         start: Date.now() - this.state.time,
+         isOn: true
+      });
+      this.timer = await setInterval(() => this.setState({
+         time: Date.now() - this.state.start
+      }), 1);
+      await console.log('timer started')
+   }
+
+   stopTimer = async () => {
+      const { time, wordCount } = this.state;
+      let seconds = time / 1000;
+      let minutes = seconds / 60;
+      let wpm = wordCount / minutes;
+      await this.setState({ isOn: false })
+      await clearInterval(this.timer)
+      console.log('timer stopped', seconds)
+      alert(wpm + ' words per minute')
+   }
+
+   resetTimer = () => {
+      this.setState({ time: 0 })
+   }
+
+
+   render() {
+      const { originalTextArray, userInput, time } = this.state;
+      // Spell checking
+      let displayParagraph = originalTextArray.map((letter, index) => {
+         if (!userInput[index]) { //if user hasn't entered the character yet, letter will be gray
+            return (
+               <span style={{ color: 'gray' }} key={index}>{letter}</span>
+            )
+         } else if (letter === userInput[index]) { //if what the user typed in matches the original text, letter will be green
+            return (
+               <span style={{ color: 'green' }} key={index}>{letter}</span>
+            )
+         } else if (letter !== userInput[index]) { //if what the user typed doesn't match the original text, letter will be red
+            return (
+               <span style={{ backgroundColor: 'red' }} key={index}>{letter}</span>
+            )
+         }
+      })
+
+      // Timer
+      let seconds = time / 1000;
+
+
+      return (
+         <div className='typing-test'>
+            <div className='text-window'>
+               {displayParagraph}
+            </div>
+            <div>
+               <h3 style={{ width: '30px' }} >Time: {seconds}</h3>
+            </div>
+            <textarea
+               className='input-box'
+               type='text'
+               onChange={(e) => this.handleInput(e.target.value)}
+            >
+
+            </textarea>
+         </div>
+      )
+   }
+}
