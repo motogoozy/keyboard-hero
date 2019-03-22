0fcfad1093bf0a1c13c3865a0cba71b6cac48e12
kspayne93 <kspayne93@gmail.com>
2019-03-21 21:41:54 -0600

    rendered TypingTest component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
+import TypingTest from '../src/TypingTest/TypingTest';
 
 class App extends Component {
+  state = {
+
+  }
+
+
   render() {
+
     return (
       <div className="App">
-
+          <div className='test-container'>
+            <TypingTest />
+          </div>
       </div>
     );
   }
