2433b1dfa920ca65914c2670e224a08d984c0ddb
kspayne93 <kspayne93@gmail.com>
2019-03-23 12:11:41 -0600

    created and styled view

---
+++ src/views/Landing/Landing.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './Landing.scss';
+import { Link } from 'react-router-dom';
+
+export default class Landing extends Component {
+   render() {
+      return (
+         <div className='landing-page'>
+            <h1>Welcome to Keyboard Hero!</h1>
+            <p>Keyboard Hero is a typing challenge that tests both your typing speed and accuracy.</p>
+            <p>Ready to begin?</p>
+            <Link to='/challenge'>Let's Go!</Link>
+         </div>
+      )
+   }
+}
