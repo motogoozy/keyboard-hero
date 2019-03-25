e09cbdaf2c15535fabcce56259d382e0a1a6ef6c
kspayne93 <kyle@investavante.com>
2019-03-25 01:08:45 -0600

    added keyboard hero name and link

--- src/views/ChallengePage/ChallengePage.jsx
+++ src/views/ChallengePage/ChallengePage.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './ChallengePage.scss';
 import TypingTest from '../../components/TypingTest/TypingTest';
+import { Link } from 'react-router-dom';
 
 class ChallengePage extends Component {
    render() {
       return (
          <div className='challenge-main'>
+            <Link to='/' style={{ textDecoration: 'none', color: 'white' }}>
+               <h1 style={{ fontSize: '3em', fontFamily: 'Courier' }}>Keyboard Hero</h1>
+            </Link>
             <div className='test-container'>
                <TypingTest />
             </div>
