d9b35979aaccbe846eceb19c5570eba14c06ef1b
kspayne93 <kspayne93@gmail.com>
2019-03-25 22:49:14 -0600

    added Leaderboard route

--- src/routes.js
+++ src/routes.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Landing from './views/Landing/Landing';
 import ChallengePage from './views/ChallengePage/ChallengePage';
+import Leaderboard from './views/Leaderboard/Leaderboard';
 
 
 export default (
    <Switch>
       <Route component={Landing} exact path='/' ></Route>
       <Route component={ChallengePage} path='/challenge' ></Route>
+      <Route component={Leaderboard} path='/leaderboard' ></Route>
    </Switch>
 )
