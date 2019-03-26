99d3438b1b1187a2ecf5fe7382d1386e957d4dc0
kspayne93 <kyle@investavante.com>
2019-03-25 22:48:59 -0600

    added score endpoints

--- server/server.js
+++ server/server.js
@@ -22,9 +22,26 @@ massive(CONNECTION_STRING).then(db => {
 })
 
 // ENDPOINTS
+
+// Get paragraph
 app.get(`/api/paragraph/:paragraph_id`, async (req, res) => {
    const { paragraph_id } = req.params;
    const db = req.app.get('db');
    const paragraphText = await db.get_paragraph({ paragraph_id });
    res.status(200).send(paragraphText);
+})
+
+// Add score
+app.post(`/api/score`, async (req, res) => {
+   const { name, score } = req.body;
+   const db = req.app.get('db');
+   const added = await db.add_score({ name, score });
+   res.status(200).send({ message: 'score added' })
+})
+
+// Get all scores
+app.get('/api/score', async (req, res) => {
+   const db = req.app.get('db');
+   const scores = await db.get_scores();
+   res.status(200).send(scores)
 })
