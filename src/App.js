69501d597a32050b6e926707cf4124d115c1f429
Kyle Payne <kspayne93@gmail.com>
2019-10-01 15:00:15 -0600

    added footer and copyright

--- src/App.js
+++ src/App.js
@@ -6,7 +6,13 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        { routes }
+        <div>
+          { routes }
+        </div>
+        <footer>
+          {/* <p className='copyright-text'>&copy; Copyright</p> */}
+          <small>&copy; 2019, Kyle Payne | All Rights Reserved</small>
+        </footer>
       </div>
     );
   }
